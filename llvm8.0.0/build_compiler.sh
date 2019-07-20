389c8e138c7e7e1c2c7c4c9b56ecb73c6934d8a9
toshihiro konda <kontoshi0317@gmail.com>
2019-07-20 11:48:38 +0900

    fix build shell

--- llvm8.0.0/build_compiler.sh
+++ llvm8.0.0/build_compiler.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+cd llvm
 mkdir build
 cd build
-cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=X86 -G "Unix Makefiles" ../llvm
+cmake -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=X86 -G "Unix Makefiles" ../
 make -j `nproc`
